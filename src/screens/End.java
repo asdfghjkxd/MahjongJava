e55d184a396b25d69316ca8bd111f7426dbe796a
George <73705042+asdfghjkxd@users.noreply.github.com>
2022-12-08 23:37:35 +0800

    Template for backtracking
    Added a new screen for the winning player

--- src/screens/End.java
+++ src/screens/End.java
@@ -1,28 +1,18 @@
 package screens;
 
 import core.Game;
-import utils.Tickable;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 
-public class Pause extends Screen implements Tickable {
-    public Pause(Game game) {
+public class End extends Screen {
+    public End(Game game) {
         super(game);
     }
 
     @Override
     public void render(Graphics g) throws IOException {
-        g.setColor(new Color(0, 80, 0));
-        g.fillRect(0, 0, game.getWidth(), game.getHeight());
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, 40));
-        g.drawString("PAUSED", 500, 400);
-        g.setColor(Color.WHITE);
-        g.fillRect(450, 475, 275, 70);
-        g.setColor(Color.BLACK);
-        g.drawString("Resume", 510, 525);
+        super.render(g);
     }
 
     @Override
